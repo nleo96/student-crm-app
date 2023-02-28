d11e332a7398699a0a7ff2ac0a5d6042987f75c0
Leonardo Leoncio <Leonardo@Intellisys.local>
2023-02-28 18:08:04 -0300

    initial commite

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container">
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await axios.post('http://localhost:8090/login', {
+          user: this.username,
+          password: this.password,
+        });
+        console.log(response.data); // handle successful login
+      } catch (error) {
+        console.error(error.response.data); // handle error
+      }
+    },
+  },
+};
+</script>
+
+
+<style>
+.container {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f7f7f7;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-group {
+  margin-bottom: 10px;
+}
+
+label {
+  font-weight: bold;
+}
+
+input[type="text"],
+input[type="password"] {
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+
+button[type="submit"] {
+  margin-top: 20px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+</style>
