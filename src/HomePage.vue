a1533c04faf1741bb061a0cfb7c5f246eddb1ab1
strhigor <higor.b97@hotmail.com>
2023-03-14 18:35:03 -0300

    Feat: HomePage definida para após login

---
+++ src/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <a href="/NewContact"><button id="newContact">Cadastrar contato</button></a>
+</template>
+
+<style>
+
+#newContact {    
+    font-size: 16px;
+    border-radius: 5px;
+    padding: 14px 25px;
+    border: none;
+    font-weight: 500;
+    background-color: #6a64f1;
+    color: white;
+    cursor: pointer;
+    margin-top: 25px;
+    margin-left: 25px;
+}
+</style>
